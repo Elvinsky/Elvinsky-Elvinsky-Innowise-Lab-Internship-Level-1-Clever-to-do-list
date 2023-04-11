4a1f57a5d8366f6bdf43ce9c5a9eed05d93474dc
Elvinsky <mikhnevichnik@gmail.com>
2023-04-11 17:53:05 +0300

    merged

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store';
-
+import TestComponent from './components/TestComponent.vue'
+const app = createApp(App);
+app.use(store);
+app.component('TestComponent',TestComponent)
 createApp(App).use(store).mount('#app');
